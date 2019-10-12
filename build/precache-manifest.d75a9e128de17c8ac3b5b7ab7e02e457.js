9db1a300ee3ff80213f0c6a5a4321849a64cb48c
Patrick Lombardo <patrick_lombardo@comcast.com>
2019-10-12 13:08:49 -0400

    building and adding node_modules to gitignore

---
+++ build/precache-manifest.d75a9e128de17c8ac3b5b7ab7e02e457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a1f45799dd75f3f394314688cf01c9",
+    "url": "/redux-blog/index.html"
+  },
+  {
+    "revision": "09bfd8d5eb6e9d87478e",
+    "url": "/redux-blog/static/css/main.2b9d1a58.chunk.css"
+  },
+  {
+    "revision": "dfadf52b22712050e962",
+    "url": "/redux-blog/static/js/2.2175ba78.chunk.js"
+  },
+  {
+    "revision": "09bfd8d5eb6e9d87478e",
+    "url": "/redux-blog/static/js/main.1a0484da.chunk.js"
+  },
+  {
+    "revision": "28fcf3a7bbea395e8c39",
+    "url": "/redux-blog/static/js/runtime~main.8b9d83e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
